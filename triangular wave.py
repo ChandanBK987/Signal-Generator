1a366b957b9840ff40020e9784b9c1a8b3d95017
ChandanBK987 <153383045+ChandanBK987@users.noreply.github.com>
2024-10-20 15:56:21 +0530

    Generating

--- triangular wave.py
+++ triangular wave.py
@@ -8,3 +8,7 @@ amplitude = 1  # Amplitude of the triangular wave
 time_period = 1 / frequency  # Time period of the wave
 sampling_rate = 1000  # Sampling rate in samples per second
 time = np.linspace(0, 2 * time_period, int(sampling_rate))  # Time values
+
+# Generating the triangular wave
+triangular_wave = amplitude * signal.sawtooth(2 * np.pi * frequency * time, 0.5)
+
