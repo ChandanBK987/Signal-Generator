8fd3eafc01b859bd34ff7dc7d25b8c176607a7f5
ChandanBK987 <153383045+ChandanBK987@users.noreply.github.com>
2024-10-20 15:46:59 +0530

    Generating wave

--- sine wave.py
+++ sine wave.py
@@ -8,4 +8,5 @@ time_period = 1 / frequency  # Time period of the sine wave
 sampling_rate = 1000  # Sampling rate in samples per second
 time = np.linspace(0, 2 * time_period, int(sampling_rate))  # Time values
 
-
+# Generating the sine wave
+sine_wave = amplitude * np.sin(2 * np.pi * frequency * time)
